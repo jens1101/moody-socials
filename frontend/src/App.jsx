ef56e984a4eedd6a7af5a2472affcc80505cf1e7
Jens Tschirpig <jens1101@users.noreply.github.com>
2021-01-21 06:47:28 +0200

    Added feed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,32 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
+import { Feed } from "./Feed";
 
 function App() {
   return (
-    <Navbar variant={"dark"} bg={"dark"} expand={"sm"}>
-      <Container>
-        <Navbar.Brand href="/">Moody Socials</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-          </Nav>
-          <Button variant="primary">Login</Button>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <div>
+      <Navbar variant={"dark"} bg={"dark"} expand={"sm"}>
+        <Container>
+          <Navbar.Brand href="/">Moody Socials</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+            </Nav>
+            <Button variant="primary">Login</Button>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      <main className={"mt-3 mb-3"}>
+        <Container>
+          <h1>Feed</h1>
+        </Container>
+        <Feed />
+      </main>
+    </div>
   );
 }
 
