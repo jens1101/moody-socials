8a48dabb038cc08243ebd28577d3c5ebb5bcc494
Jens Tschirpig <jens1101@users.noreply.github.com>
2021-01-21 08:31:39 +0200

    Added validation to the login and register screens

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 export function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [validated, setValidated] = useState(false);
+
+  function onLogin(event) {
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  }
+
   return (
     <Container className={"text-center"}>
       <Card bg={"light"}>
@@ -12,16 +28,26 @@ export function Login() {
             <h1>Login</h1>
           </Card.Title>
 
-          <Form>
+          <Form onSubmit={onLogin} noValidate validated={validated}>
             <div className={"text-left"}>
               <Form.Group controlId={"loginUsername"}>
                 <Form.Label>Username</Form.Label>
-                <Form.Control type={"text"} />
+                <Form.Control
+                  type={"text"}
+                  required
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                />
               </Form.Group>
 
               <Form.Group controlId={"loginPassword"}>
                 <Form.Label>Password</Form.Label>
-                <Form.Control type={"password"} />
+                <Form.Control
+                  type={"password"}
+                  required
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
               </Form.Group>
             </div>
 
--- frontend/src/Register.jsx
+++ frontend/src/Register.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 export function Register() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [validated, setValidated] = useState(false);
+
+  function onRegister(event) {
+    const form = event.currentTarget;
+
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  }
+
   return (
     <Container className={"text-center"}>
       <Card bg={"light"}>
@@ -12,21 +29,52 @@ export function Register() {
             <h1>Register</h1>
           </Card.Title>
 
-          <Form>
+          <Form onSubmit={onRegister} noValidate validated={validated}>
             <div className={"text-left"}>
               <Form.Group controlId={"registerUsername"}>
                 <Form.Label>Username</Form.Label>
-                <Form.Control type={"text"} />
+                <Form.Control
+                  required
+                  type={"text"}
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                />
               </Form.Group>
 
               <Form.Group controlId={"registerPassword"}>
                 <Form.Label>Password</Form.Label>
-                <Form.Control type={"password"} />
+                <Form.Control
+                  required
+                  type={"password"}
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
               </Form.Group>
 
               <Form.Group controlId={"registerConfirmPassword"}>
                 <Form.Label>Confirm password</Form.Label>
-                <Form.Control type={"password"} />
+                <Form.Control
+                  required
+                  type={"password"}
+                  value={confirmPassword}
+                  isValid={false}
+                  onChange={(event) => {
+                    const inputElement = event.target;
+
+                    setConfirmPassword(inputElement.value);
+
+                    // This uses the constraint validation API to set this field
+                    // as either valid or invalid.
+                    inputElement.setCustomValidity(
+                      inputElement.value !== password
+                        ? "Passwords don't match"
+                        : ""
+                    );
+                  }}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Passwords don't match
+                </Form.Control.Feedback>
               </Form.Group>
             </div>
 
