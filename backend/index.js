87e8a66389dec6226900112bed60ced4234cd2ae
Jens Tschirpig <jens1101@users.noreply.github.com>
2021-01-28 21:50:56 +0200

    [WIP] Fixing conflicting backend code

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const router = require('./router');
-const bodyParser = require('body-parser')
-const cors = require('cors');
+const express = require("express");
+const router = require("./router");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 const app = express();
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-});
-
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: process.env.COOKIE_MAX_AGE_MS,
+    },
+  })
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
