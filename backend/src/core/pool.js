ba81ecd106e2d49ade4b1bf2dec647fce0cadeec
Jens Tschirpig <jens1101@users.noreply.github.com>
2021-01-28 22:09:20 +0200

    Removed unnecessary dependencies

--- backend/src/core/pool.js
+++ backend/src/core/pool.js
@@ -1,6 +1,5 @@
-const util = require("util");
+const { Pool } = require("pg");
 
-const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "dbsjffgqesetwa",
   host: "ec2-18-235-109-97.compute-1.amazonaws.com",
@@ -8,7 +7,5 @@ const pool = new Pool({
   password: "1fe1fb24710724998f33af35c8b79f48134cf30a979b5e93178459868ba26b6f",
   port: 5432,
 });
-
-pool.query = util.promisify(pool.query);
 
 module.exports = pool;
